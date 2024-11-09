79d0b3b361322c7a8b11b4bd8d4a0a575a32ef63
preanto <preanto15-5059@diu.edu.bd>
2024-11-10 00:25:18 +0600

    Output 1-10

---
+++ loop1.sh
@@ -0,0 +1,23 @@
+#Problem Statement ----->
+
+# Write a C program to print 1-10.
+
+# Sample Output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+#Code starts here ---->
+
+#!/bin/bash
+for i in {1..10}
+do
+echo $i
+done
